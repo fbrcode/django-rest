bff7babb75c5d4dcdf0b9e80a1a93f5b36c4a7a7
fbrcode <fabio.bressler@gmail.com>
2023-01-09 10:36:50 -0300

    replace setting by env vars

--- drf/app/settings.py
+++ drf/app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*#l)i1%%w9$l&v&6dcmhrk@gw$i(@u83o1*7h48d*umf(pm=py"
+#SECRET_KEY = "django-insecure-*#l)i1%%w9$l&v&6dcmhrk@gw$i(@u83o1*7h48d*umf(pm=py"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
-
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
