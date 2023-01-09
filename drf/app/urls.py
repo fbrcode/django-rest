dd337b3c579d81149a10d844b88805c158c635d1
fbrcode <fabio.bressler@gmail.com>
2023-01-09 17:33:01 -0300

    add routing to the application

--- drf/app/urls.py
+++ drf/app/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+urlpatterns = router.urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
